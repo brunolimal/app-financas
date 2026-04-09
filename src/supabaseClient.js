9809616bb87af4c4888710888958a736a1882d50
Bruno Andrade <seu-email@gmail.com>
2026-04-08 21:35:54 -0300

    Sistema corrigido 100%

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,6 @@
 ﻿import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://eibtfztgxfivvkmsjarq.supabase.co' // Pegue no menu Data API
+
+const supabaseUrl = 'https://eibtfztgxfivvkmsjarq.supabase.co'
 const supabaseAnonKey = 'sb_publishable_iAZmXewR1Fi1A_ZRxx2ABQ__ip-E7Hk'
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
